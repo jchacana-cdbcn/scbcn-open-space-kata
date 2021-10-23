2176e311349aea03b353a0e5d639e3e5af28f392
Javier Chacana <javier.chacana@codurance.com>
2021-10-23 16:35:14 +0200

    mob next [ci-skip]

--- src/test/roman-normal.test.ts
+++ src/test/roman-normal.test.ts
@@ -1,5 +1,12 @@
+import {romanConverter} from "../main/roman-normal.test";
+
 describe('Roman Numerals', () => {
     it('should return `` when passing 0', () => {
         expect(romanConverter(0)).toBe('')
     })
+
+    it('should return `I` when passing 1', () => {
+        expect(romanConverter(1)).toBe('I')
+    })
+
 })
